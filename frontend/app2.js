2b3c39acdd6e3e1839b846d48ffffaceab3b508d
Devin Bedi <bedidevin@gmail.com>
2025-06-16 23:29:12 +0530

    Update BASE_URL to deployed backend URL

--- frontend/app2.js
+++ frontend/app2.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:5000/api';
+const BASE_URL = 'https://factfront.onrender.com/api'; 
 let token = localStorage.getItem('token');
 
 // ============ MESSAGE BAR ============ //
