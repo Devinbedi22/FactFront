f1f979e7ba2220faae486e831d662402aa5d4bf0
Devin Bedi <bedidevin@gmail.com>
2025-06-19 21:23:10 +0530

    backend:up

--- backend/server2.js
+++ backend/server2.js
@@ -9,6 +9,7 @@ require('dotenv').config();
 
 /* ---------- App init ---------- */
 const app  = express();
+app.set('trust proxy', 1);
 const PORT = process.env.PORT || 5000;
 const isProduction = process.env.NODE_ENV === 'production';
 
